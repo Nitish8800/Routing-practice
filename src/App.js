1db18558e1423f90b4057fd026505aa8982dbd0e
Nitish Kumar <81190422+Nitish8800@users.noreply.github.com>
2022-03-20 21:13:54 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import { Home } from "./Components/Home";
+import { About } from "./Components/About";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Home />
+      <About />
+    </div>
+  );
+}
---
+++ src/Components/About.jsx
@@ -0,0 +1,8 @@
+export const About = () => {
+  return (
+    <div>
+      About us :<p>We are best</p>
+      <p>Thanks</p>
+    </div>
+  );
+};
