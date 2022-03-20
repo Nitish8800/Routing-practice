bade001da1005c269622850fa375f80284c10f80
Nitish Kumar <81190422+Nitish8800@users.noreply.github.com>
2022-03-20 21:49:00 +0530

    commit

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,8 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 export const About = () => {
+  const Navigate = useNavigate();
   return (
     <div>
       About us :<p>We are best</p>
       <p>Thanks</p>
+      <button
+        onClick={() => {
+          Navigate("/product/refrigirator");
+        }}
+      >
+        Submit
+      </button>
     </div>
   );
 };
